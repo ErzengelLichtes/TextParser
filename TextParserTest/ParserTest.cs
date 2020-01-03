b6a8b424e6941a4d16734828a81c0c7045ab2f35
ErzengelLichtes <erzengel.des.lichtes@gmail.com>
2020-01-03 11:16:49 -0800

    Split "Expect" tests

--- TextParserTest/ParserTest.cs
+++ TextParserTest/ParserTest.cs
@@ -28,28 +28,6 @@ namespace TextParserTest
 
 
         [TestMethod]
-        public void StringExpectFirstTwoCharacters()
-        {
-            var p = CreateReader("abc");
-            p.Expect("ab");
-            AssertCharacterPosition(new CharacterPosition(1, 3), p.CharacterPosition);
-        }
-        [TestMethod]
-        public void StringExpectWrongCharactersThrows()
-        {
-            var p = CreateReader("abc");
-            try
-            {
-                p.Expect("c");
-                Assert.Fail("Expected exception");
-            }
-            catch (CompilerException e)
-            {
-                Assert.AreEqual("Expected c", e.ErrorMessage);
-                AssertCharacterPosition(new CharacterPosition(1, 1), e.CharacterPosition);
-            }
-        }
-        [TestMethod]
         public void StringWordIdentifierNone()
         {
             var p = CreateReader("{abc}");
---
+++ TextParserTest/ParserTests/Expect.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextParser;
+using static TextParserTest.Helper;
+
+namespace TextParserTest.ParserTests
+{
+    [TestClass]
+    public class Expect
+    {
+        [TestMethod]
+        public void StringExpectFirstTwoCharacters()
+        {
+            var p = CreateReader("abc");
+            p.Expect("ab");
+            AssertCharacterPosition(new CharacterPosition(1, 3), p.CharacterPosition);
+        }
+        [TestMethod]
+        public void StringExpectWrongCharactersThrows()
+        {
+            var p = CreateReader("abc");
+            try
+            {
+                p.Expect("c");
+                Assert.Fail("Expected exception");
+            }
+            catch (CompilerException e)
+            {
+                Assert.AreEqual("Expected c", e.ErrorMessage);
+                AssertCharacterPosition(new CharacterPosition(1, 1), e.CharacterPosition);
+            }
+        }
+    }
+}
