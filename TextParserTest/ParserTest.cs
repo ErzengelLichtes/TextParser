5abcd617debb0e52c2c290760f8d96b553cf88e0
ErzengelLichtes <erzengel.des.lichtes@gmail.com>
2020-01-03 11:18:31 -0800

    Split "PeekWord" tests

--- TextParserTest/ParserTest.cs
+++ TextParserTest/ParserTest.cs
@@ -28,36 +28,6 @@ namespace TextParserTest
 
 
         [TestMethod]
-        public void StringWordIdentifierNone()
-        {
-            var p = CreateReader("{abc}");
-            var ident = p.PeekWord();
-            Assert.IsNull(ident);
-            AssertCharacterPosition(new CharacterPosition(1, 1), p.CharacterPosition);
-        }
-        [TestMethod]
-        public void StringWordIdentifier()
-        {
-            var p = CreateReader("abc");
-            var ident = p.PeekWord();
-            Assert.IsNotNull(ident);
-            Assert.AreEqual("abc", ident.ToString());
-            AssertCharacterPosition(new CharacterPosition(1, 1), p.CharacterPosition, "start");
-            ident.Pop();
-            AssertCharacterPosition(new CharacterPosition(1, 4), p.CharacterPosition, "end");
-        }
-        [TestMethod]
-        public void StringWordIdentifierWithExtra()
-        {
-            var p = CreateReader("abc stuff");
-            var ident = p.PeekWord();
-            Assert.IsNotNull(ident);
-            Assert.AreEqual("abc", ident.ToString());
-            AssertCharacterPosition(new CharacterPosition(1, 1), p.CharacterPosition, "start");
-            ident.Pop();
-            AssertCharacterPosition(new CharacterPosition(1, 4), p.CharacterPosition, "end");
-        }
-        [TestMethod]
         public void StringFilenameIdentifierNone()
         {
             var p = CreateReader("<abc.def>");
---
+++ TextParserTest/ParserTests/PeekWord.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextParser;
+using static TextParserTest.Helper;
+
+namespace TextParserTest.ParserTests
+{
+    [TestClass]
+    public class PeekWord
+    {
+        [TestMethod]
+        public void StringWordIdentifierNone()
+        {
+            var p     = CreateReader("{abc}");
+            var ident = p.PeekWord();
+            Assert.IsNull(ident);
+            AssertCharacterPosition(new CharacterPosition(1, 1), p.CharacterPosition);
+        }
+        [TestMethod]
+        public void StringWordIdentifier()
+        {
+            var p     = CreateReader("abc");
+            var ident = p.PeekWord();
+            Assert.IsNotNull(ident);
+            Assert.AreEqual("abc", ident.ToString());
+            AssertCharacterPosition(new CharacterPosition(1, 1), p.CharacterPosition, "start");
+            ident.Pop();
+            AssertCharacterPosition(new CharacterPosition(1, 4), p.CharacterPosition, "end");
+        }
+        [TestMethod]
+        public void StringWordIdentifierWithExtra()
+        {
+            var p     = CreateReader("abc stuff");
+            var ident = p.PeekWord();
+            Assert.IsNotNull(ident);
+            Assert.AreEqual("abc", ident.ToString());
+            AssertCharacterPosition(new CharacterPosition(1, 1), p.CharacterPosition, "start");
+            ident.Pop();
+            AssertCharacterPosition(new CharacterPosition(1, 4), p.CharacterPosition, "end");
+        }
+    }
+}
