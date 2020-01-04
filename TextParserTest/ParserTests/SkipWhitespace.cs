eaedc2c95f24d086719a1bfb748764bd2efed86d
ErzengelLichtes <erzengel.des.lichtes@gmail.com>
2020-01-04 10:15:42 -0800

    Added skip whitespae to newline tests

--- TextParserTest/ParserTests/SkipWhitespace.cs
+++ TextParserTest/ParserTests/SkipWhitespace.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TextParser;
 using static TextParserTest.Helper;
@@ -38,6 +39,22 @@ namespace TextParserTest.ParserTests
             var word2 = p.ReadWord();
             Assert.AreEqual("abc", word);
             Assert.AreEqual("and", word2);
+        }
+
+        [TestMethod]
+        [DataRow("no_ws", Parser.IsNewline.NoWhitespace, 6)]
+        [DataRow("ws_before    \ntest", Parser.IsNewline.AtNewline, 14)]
+        [DataRow("ws_before_after    \n    test", Parser.IsNewline.AtNewline, 20)]
+        [DataRow("ws_no_line\tnl_follows\n    test", Parser.IsNewline.WhitespaceOnly, 12)]
+        public void SkipWhitespaceToNewline(string lines, Parser.IsNewline expected, int character)
+        {
+            // ReSharper disable MustUseReturnValue
+            var p = CreateReader(lines);
+            p.ReadCStyleIdentifier();
+            var r = p.SkipWhitespaceToNewline();
+            // ReSharper restore MustUseReturnValue
+            Assert.AreEqual(expected, r);
+            AssertCharacterPosition(new CharacterPosition(1, character), p.CharacterPosition);
         }
     }
 }
