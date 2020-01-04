bb59f2ccc1021f14994ba8e837d8bf73335a4d97
ErzengelLichtes <erzengel.des.lichtes@gmail.com>
2020-01-03 18:30:45 -0800

    Added tests for compileexception

---
+++ TextParserTest/ParserTests/CompileException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextParser;
+using static TextParserTest.Helper;
+
+namespace TextParserTest.ParserTests
+{
+    [TestClass]
+    public class CompileException
+    {
+        [TestMethod]
+        public void Start()
+        {
+            var p = CreateReader("a");
+            var r = p.CompileException("test");
+            Assert.AreEqual("test", r.ErrorMessage);
+            AssertCharacterPosition(new CharacterPosition(1,1), r.CharacterPosition);
+        }
+        [TestMethod]
+        public void Mid()
+        {
+            var p = CreateReader("a");
+            p.Pop(1);
+            var r = p.CompileException("msg");
+            Assert.AreEqual("msg", r.ErrorMessage);
+            AssertCharacterPosition(new CharacterPosition(1, 2), r.CharacterPosition);
+        }
+        [TestMethod]
+        public void CharacterPosNewline()
+        {
+            var p = CreateReader("abc\ndef");
+            var r = p.GetCharacterPositionAfter(4);
+            AssertCharacterPosition(new CharacterPosition(2, 1), r);
+        }
+    }
+}
