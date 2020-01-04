627038a34844eda21c78548e8c70ba3a6310ebfd
ErzengelLichtes <erzengel.des.lichtes@gmail.com>
2020-01-03 18:30:36 -0800

    Added expect override for dictionary.

--- TextParserTest/ParserTests/Expect.cs
+++ TextParserTest/ParserTests/Expect.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TextParser;
 using static TextParserTest.Helper;
@@ -27,6 +28,55 @@ namespace TextParserTest.ParserTests
             catch (CompilerException e)
             {
                 Assert.AreEqual("Expected c", e.ErrorMessage);
+                AssertCharacterPosition(new CharacterPosition(1, 1), e.CharacterPosition);
+            }
+        }
+        [TestMethod]
+        public void ExpectDictionaryOne()
+        {
+            var p = CreateReader("foo");
+            int r = 0;
+            p.Expect(new Dictionary<string, Action<Parser>>()
+                     {
+                         ["foo"] = parser => r = 1,
+                         ["bar"] = parser => r = 2,
+                     });
+            Assert.AreEqual(1, r, "No or incorrect callback");
+            AssertCharacterPosition(new CharacterPosition(1, 4), p.CharacterPosition);
+        }
+        [TestMethod]
+        public void ExpectDictionaryTwo()
+        {
+            var p = CreateReader("bar");
+            int r = 0;
+            p.Expect(new Dictionary<string, Action<Parser>>()
+                     {
+                         ["foo"] = parser => r = 1,
+                         ["bar"] = parser => r = 2,
+                     });
+            Assert.AreEqual(2, r, "No or incorrect callback");
+            AssertCharacterPosition(new CharacterPosition(1, 4), p.CharacterPosition);
+        }
+        [TestMethod]
+        public void ExpectDictionaryBad()
+        {
+            var p = CreateReader("abc");
+            try
+            {
+            #pragma warning disable 219
+                int r = 0;
+            #pragma warning restore 219
+                p.Expect(new Dictionary<string, Action<Parser>>()
+                         {
+                             ["foo"] = parser => r = 1,
+                             ["bar"] = parser => r = 2,
+                         });
+                Assert.Fail("Expected exception");
+            }
+            catch (CompilerException e)
+            {
+                StringAssert.Contains(e.ErrorMessage, "foo");
+                StringAssert.Contains(e.ErrorMessage, "bar");
                 AssertCharacterPosition(new CharacterPosition(1, 1), e.CharacterPosition);
             }
         }
